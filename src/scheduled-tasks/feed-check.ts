dabb784b74256b853a1093a357df2713209e7772
Excigma <30280397+Excigma@users.noreply.github.com>
2024-07-15 13:49:51 +1200

    fix: used wrong envvar

--- src/scheduled-tasks/feed-check.ts
+++ src/scheduled-tasks/feed-check.ts
@@ -14,7 +14,7 @@ interface ConfigCourses {
   rssUrls: string[];
 }
 
-const checkInterval = process.env.INTERVAL ? parseInt(process.env.INTERVAL) : Time.Minute * 15;
+const checkInterval = process.env.CHECK_INTERVAL ? parseInt(process.env.CHECK_INTERVAL) : Time.Minute * 15;
 
 @ApplyOptions<ScheduledTask.Options>({
   name: 'feed-check',
